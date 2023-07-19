3015d1812f15397cd6f8fc9901b38863cc3a8689
Everett Southwick <everett@southwick.dev>
2023-07-18 20:47:27 -0400

    match /t/

--- src/routes/threadRoutes.ts
+++ src/routes/threadRoutes.ts
@@ -4,7 +4,27 @@ import { errorTemplate, postTemplate } from '../templates/templates';
 
 const router = express.Router();
 
-router.get('/:username/post/*', async (req, res) => {
+router.get('/t/:shortcode', async (req, res) => {
+    const threadsPath = req.path;
+
+    try {
+        const post = await getPost(threadsPath);
+
+        const metadata = generateMetadata(post, threadsPath);
+        const html = postTemplate(
+            metadata.metadata
+        );
+
+        return res.status(200).send(html);
+    }
+    catch (error: any) {
+        console.error(error);
+        const html = errorTemplate(error.status, error.statusText, `https://threads.net${threadsPath}`);
+        return res.status(200).send(html);
+    }
+});
+
+router.get('/:username/post/:shortcode', async (req, res) => {
     const threadsPath = req.path;
 
     try {
